40ff58f1e81691c8beb6b6586cdf08f0a7044ccb
Gean Gomes <geansg4@gmail.com>
2022-02-21 12:52:42 -0300

    chore: add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.homeContainer}>
+        <section className={styles.homeContent}>
+          <span>Hey, welcome</span>
+          <h1>New about the <span>React</span> world</h1>
+          <p>
+            Get acess to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width={750}
+          height={750}          
+        />
+
+      </main>
     </>
   )
 }
