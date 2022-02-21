338df4d0dc803901560bf992a4bca00f283c03c5
Gean Gomes <geansg4@gmail.com>
2022-02-21 12:21:41 -0300

    chore: move public to root

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
-import styles from './styles.module.scss'
+import Image from 'next/image'
 
-import LogoSvg from '../../public/images/logo.svg'
+import styles from './styles.module.scss'
 import { SignInButton } from '../SignInButton'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <LogoSvg />
+        <Image
+          src="/images/logo.svg"
+          alt="ig.news"
+          width="100%"
+          height="100%"
+        />
 
         <nav>
           <a className={styles.active}>Home</a>
