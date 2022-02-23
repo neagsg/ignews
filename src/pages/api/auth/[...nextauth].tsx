bf63e53dfa2d12a241809e20289c4de00ee016b4
Gean Gomes <geansg4@gmail.com>
2022-02-23 19:58:14 -0300

    chore: add save email in faunadb

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb'
+// import { FaunaAdapter } from '@next-auth/fauna-adapter'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,19 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  // adapter: FaunaAdapter(fauna),
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const { email: userEmail } = user
+
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { userEmail } }
+          )
+        )
+      return true
+    }
+  }
 })
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,4 +2,7 @@ import { Client } from "faunadb";
 
 export const fauna = new Client({
   secret: process.env.FAUNADB_KEY,
+  // domain: "db.fauna.com",
+  // scheme: "https",
+  // port: 443,
 });
